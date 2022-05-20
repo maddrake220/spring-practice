9625cef8fad0b386e68eb4e12b6060ba27e4a692
maddrake220 <jwchoi4118@gmail.com>
2022-05-21 04:23:33 +0900

    feat) MemberRepository

---
+++ src/main/java/maddrake/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package maddrake.hellospring.repository;
+
+import maddrake.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
