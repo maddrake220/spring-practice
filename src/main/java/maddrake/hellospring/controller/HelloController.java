dc0c8ffb1a8faf61338581afc819f39325a7a98f
maddrake220 <jwchoi4118@gmail.com>
2022-05-21 04:06:07 +0900

    feat) spring 기초 static, MVC, api(string, json)

---
+++ src/main/java/maddrake/hellospring/controller/HelloController.java
@@ -0,0 +1,53 @@
+package maddrake.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // /hello 하면 이 메서드 호출
+    @GetMapping("hello")
+    public String hello(Model model) {
+        // Model MVC에서 Model
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // ResponseBody를 만나면 http에 그대로 넘김
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        // 객체가 오면 default 로 JsonConverter를 통해 json으로 넘겨줌
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
