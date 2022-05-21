970a5783eef2e41a7d949ec3e95f7335b19d234f
maddrake220 <jwchoi4118@gmail.com>
2022-05-21 14:59:20 +0900

    feat) member join by template

---
+++ src/main/java/maddrake/hellospring/controller/HomeController.java
@@ -0,0 +1,13 @@
+package maddrake.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
